4bf15739b962c26de0f4681bdf7fb61598d7e113
User <User@LAB3-3>
2024-01-24 09:33:58 +0200

    binding propertyChange

--- binding/MainPage.xaml.cs
+++ binding/MainPage.xaml.cs
@@ -1,14 +1,19 @@
-﻿namespace binding
-{
-    public partial class MainPage : ContentPage
-    {
+﻿using binding.Models;
+namespace binding;
+ public partial class MainPage : ContentPage
+ {
+    Student s;
    
 
         public MainPage()
         {
+            s =new Student() {Name="Tami", Age=16 };
+            this.BindingContext = s;
             InitializeComponent();
         }
 
-     
+    private void ChangeName(object sender, EventArgs e)
+    {
+        s.Name = "Ziv";
     }
 }
---
+++ binding/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binding.Models
+{
+    public class Student: INotifyPropertyChanged
+    {
+        private string name;
+        private int age;
+        public string Name {  get => name;set { name = value; OnPropertychanged(); } }
+        public int Age { get=>age; set { age = value; OnPropertychanged(); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertychanged([CallerMemberName]string propertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
